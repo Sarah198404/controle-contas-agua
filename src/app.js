aee63f4793af5a174850e4a25684772beea90584
Sarah <sarah.santos@aluno.faculdadeimpacta.com.br>
2024-09-14 13:29:55 -0300

    adicionando tratamento cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const lojaRoutes = require('./routes/lojaRoutes');
 const contaRoutes = require('./routes/contaRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/lojas', lojaRoutes);
